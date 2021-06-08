570b6f6e6a12aa1f7097dd8574c0c62f516da0a4
Daniel Madalitso Phiri <malgamves@gmail.com>
2021-06-08 11:32:54 +0200

    fixes

--- template/config/functions/bootstrap.js
+++ template/config/functions/bootstrap.js
@@ -3,120 +3,121 @@
 const fs = require("fs");
 const path = require("path");
 const mime = require("mime-types");
-const {
-    questions }
-        = require("../../data/data.json");
+const { questions } = require("../../data/data.json");
 
 async function isFirstRun() {
-    const pluginStore = strapi.store({
-        environment: strapi.config.environment,
-        type: "type",
-        name: "setup",
-    });
-    const initHasRun = await pluginStore.get({ key: "initHasRun" });
-    await pluginStore.set({ key: "initHasRun", value: true });
-    return !initHasRun;
-};
+  const pluginStore = strapi.store({
+    environment: strapi.config.environment,
+    type: "type",
+    name: "setup",
+  });
+  const initHasRun = await pluginStore.get({ key: "initHasRun" });
+  await pluginStore.set({ key: "initHasRun", value: true });
+  return !initHasRun;
+}
 
 async function setPublicPermissions(newPermissions) {
-    // Find the ID of the public role
-    const publicRole = await strapi
-        .query("role", "users-permissions")
-        .findOne({ type: "public" });
+  // Find the ID of the public role
+  const publicRole = await strapi
+    .query("role", "users-permissions")
+    .findOne({ type: "public" });
 
-    // List all available permissions
-    const publicPermissions = await strapi
+  // List all available permissions
+  const publicPermissions = await strapi
+    .query("permission", "users-permissions")
+    .find({
+      type: ["users-permissions", "application"],
+      role: publicRole.id,
+    });
+
+  // Update permission to match new config
+  const controllersToUpdate = Object.keys(newPermissions);
+  const updatePromises = publicPermissions
+    .filter((permission) => {
+      // Only update permissions included in newConfig
+      if (!controllersToUpdate.includes(permission.controller)) {
+        return false;
+      }
+      if (!newPermissions[permission.controller].includes(permission.action)) {
+        return false;
+      }
+      return true;
+    })
+    .map((permission) => {
+      // Enable the selected permissions
+      return strapi
         .query("permission", "users-permissions")
-        .find({
-            type: ["users-permissions", "application"],
-            role: publicRole.id,
-        });
-
-    // Update permission to match new config
-    const controllersToUpdate = Object.keys(newPermissions);
-    const updatePromises = publicPermissions
-        .filter((permission) => {
-            // Only update permissions included in newConfig
-            if (!controllersToUpdate.includes(permission.controller)) {
-                return false;
-            }
-            if (!newPermissions[permission.controller].includes(permission.action)) {
-                return false;
-            }
-            return true;
-        })
-        .map((permission) => {
-            // Enable the selected permissions
-            return strapi
-                .query("permission", "users-permissions")
-                .update({ id: permission.id }, { enabled: true })
-        });
-    await Promise.all(updatePromises);
+        .update({ id: permission.id }, { enabled: true });
+    });
+  await Promise.all(updatePromises);
 }
 
 function getFileSizeInBytes(filePath) {
-    const stats = fs.statSync(filePath);
-    const fileSizeInBytes = stats["size"];
-    return fileSizeInBytes;
-};
+  const stats = fs.statSync(filePath);
+  const fileSizeInBytes = stats["size"];
+  return fileSizeInBytes;
+}
 
 function getFileData(fileName) {
-    const filePath = `./data/uploads/${fileName}`;
+  const filePath = `./data/uploads/${fileName}`;
 
-    // Parse the file metadata
-    const size = getFileSizeInBytes(filePath);
-    const ext = fileName.split(".").pop();
-    const mimeType = mime.lookup(ext);
+  // Parse the file metadata
+  const size = getFileSizeInBytes(filePath);
+  const ext = fileName.split(".").pop();
+  const mimeType = mime.lookup(ext);
 
-    return {
-        path: filePath,
-        name: fileName,
-        size,
-        type: mimeType,
-    }
+  return {
+    path: filePath,
+    name: fileName,
+    size,
+    type: mimeType,
+  };
 }
 
 // Create an entry and attach files if there are any
 async function createEntry({ model, entry, files }) {
-    try {
-        const createdEntry = await strapi.query(model).create(entry);
-        if (files) {
-            await strapi.entityService.uploadFiles(createdEntry, files, {
-                model,
-            });
-        }
-    } catch (e) {
-        console.log('model', entry, e);
+  try {
+    const createdEntry = await strapi.query(model).create(entry);
+    if (files) {
+      await strapi.entityService.uploadFiles(createdEntry, files, {
+        model,
+      });
     }
+  } catch (e) {
+    console.log("model", entry, e);
+  }
 }
 
 async function importQuestions() {
-    return Promise.all(questions.map((question) => {
-        return createEntry({ model: "questions", entry: question });
-    }));
+  console.log("hello", questions);
+  return Promise.all(
+    questions.map((question) => {
+      return createEntry({ model: "questions", entry: question });
+    })
+  );
 }
 
 async function importSeedData() {
-    // Allow read of application content types
-    await setPublicPermissions({
-        questions: ['find', 'findone'],
-    });
+  // Allow read of application content types
+  await setPublicPermissions({
+    questions: ["find", "findone"],
+  });
 
-    // Create all entries
-    await importQuestions();
+  // Create all entries
+  await importQuestions();
 }
 
 module.exports = async () => {
-    const shouldImportSeedData = await isFirstRun();
+  const shouldImportSeedData = await isFirstRun();
 
-    if (shouldImportSeedData) {
-        try {
-            console.log('Setting up the template...');
-            await importSeedData();
-            console.log('Ready to go');
-        } catch (error) {
-            console.log('Could not import seed data');
-            console.error(error);
-        }
+  if (shouldImportSeedData) {
+    try {
+      console.log("Setting up the template...");
+      await importSeedData();
+      console.log("Ready to go");
+    } catch (error) {
+      console.log("Could not import seed data");
+      console.error(error);
     }
+  }
 };
